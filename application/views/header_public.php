874a822429c5a1a8637bc6bd7ecd9b79800f8192
Snacks <Armadillo@Armadillo.local>
2013-02-22 13:29:32 -0800

    added blog link

--- application/views/header_public.php
+++ application/views/header_public.php
@@ -8,7 +8,7 @@
 <a href="bio" class="<?echo latticeview::withinSubtree('bio')?'active':'';?>">bio</a>
 <a href="reviews" class="<?echo latticeview::withinSubtree('reviews')?'active':'';?>">reviews</a>
 <a href="http://www.smashwords.com/books/view/285929">buy the book</a>
-<a href="blog">blog</a>
+<a href="http://devondrake.wordpress.com/">blog</a>
 <a href="faq" class="<?echo latticeview::withinSubtree('faq')?'active':'';?>">FAQ</a>
 </div>
 
